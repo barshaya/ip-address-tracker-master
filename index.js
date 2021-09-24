388211e0b7461418bf700991eb0f67a45249eb80
barshaya <barshaya97@gmail.com>
2021-09-24 17:11:28 +0300

    desktop view

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-var mymap = L.map('mapid').setView([0,0], 1);
-L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=MEpxT42PsaWXDR1KXesP', {
-    attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
-}).addTo(mymap);
+let mymap = L.map("mapid");
+mymap.setView([0, 0], 3);
+L.tileLayer(
+  "https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=KIgo2TvlDDD69kwl8OUo",
+  {
+    attribution: `<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>`,
+  }
+).addTo(mymap);
+
 
 // Icon options
 var iconOptions = {
@@ -19,8 +24,6 @@ var iconOptions = {
     icon: customIcon
  }
 
-var marker=L.marker([0,0],markerOptions).addTo(mymap);
-
 var submit = document.getElementsByClassName('btn-submit')[0];
 submit.addEventListener('click',geocode);
 
@@ -35,8 +38,6 @@ function geocode(){
         
         var lat=response.data.location.lat;
         var lng=response.data.location.lng;
-      
-
         var result = document.querySelectorAll('#result')[0]
         result.classList.add('result');
         result.innerHTML = ` 
@@ -46,7 +47,8 @@ function geocode(){
         </span>
         <span class="result-item">
             <h2>LOCATION</h2>
-            <div>${response.data.location.city}, ${response.data.location.region}, ${response.data.location.postalCode}</div>
+            <div>${response.data.location.city}, ${response.data.location.region}</div>
+            <div> ${response.data.location.postalCode}</div>
         </span>
         <span class="result-item">
             <h2>TIMEZONE</h2>
@@ -57,7 +59,9 @@ function geocode(){
             <div>${response.data.isp}</div>
         </span>
         `
-        marker.setLatLng([lat,lng],1)
+        mymap.flyTo([lat, lng], 13);
+        marker.setLatLng([lat,lng],12)
+
       
     }).catch(function (error) {
         console.log(error);        
